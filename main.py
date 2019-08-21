801a5a8b37bb8f20264cbb8fd02f1409ac25576b
Ssect0r <wojasinskipi@gmail.com>
2019-08-21 11:36:01 +0200

    Import modules

---
+++ main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+from tensorflow import keras
+from keras.datasets import mnist
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
